13178edb40060b52efc57a319fa90cf47e20b9a8
joller134 <rugbykid10@hotmail.com>
2017-09-09 22:01:53 +0200

    Added button support

--- public/javascripts/interface.js
+++ public/javascripts/interface.js
@@ -5,6 +5,14 @@ var fs = require('fs');
 var currentScreen = 0;
 var LCD = require('lcdi2c');
 var lcd = new LCD( 1,0x3f, 16, 2);
+var Gpio = require('pigpio').Gpio,
+var screens = {
+    0: function(){lcd.println(gegevens.min + " tot " + gegevens.max + "        ",1); lcd.println("Wekker op " + gegevens.wekker + "         ",2);},
+    1: function(){lcd.println("Zon op: " + gegevens.vandaagOp + "           ",1); lcd.println("Zon on: " + gegevens.vandaagOn + "           ", 2)},
+    2: function(){lcd.println(vakken(gegevens)[0] + "                                          ",1); lcd.println(vakken(gegevens)[1] + "                              ",2)},
+"stop": function(){lcd.clear(); lcd.off()},
+"start": function(){lcd.clear(); lcd.on()}
+}
 
 var start = function(tijdenDB){
 var gegevens = tijdenDB;
@@ -20,13 +28,6 @@ var gegevens = JSON.parse(fs.readFileSync('public/DB.json', 'utf8'));
 var input = d.toString().trim();
 currentScreen = input;
 console.log("currentScreen: ", currentScreen);
-    screens = {
-        0: function(){lcd.println(gegevens.min + " tot " + gegevens.max + "        ",1); lcd.println("Wekker op " + gegevens.wekker + "         ",2);},
-        1: function(){lcd.println("Zon op: " + gegevens.vandaagOp + "           ",1); lcd.println("Zon on: " + gegevens.vandaagOn + "           ", 2)},
-        2: function(){lcd.println(vakken(gegevens)[0] + "                                          ",1); lcd.println(vakken(gegevens)[1] + "                              ",2)},
-	"stop": function(){lcd.clear(); lcd.off()},
-	"start": function(){lcd.clear(); lcd.on()}
-    }
     screens[currentScreen]();
 });
 
@@ -65,5 +66,62 @@ var vakken = function(gegevens){
     var ln = [ln1,ln2];
     return ln;
 }
+
+
+//--------- I/O ---------//
+
+
+button = new Gpio(4, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_UP,
+    edge: Gpio.EITHER_EDGE
+  });
+  
+  button1 = new Gpio(17, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_UP,
+    edge: Gpio.EITHER_EDGE
+  });
+  
+    button2 = new Gpio(27, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_UP,
+    edge: Gpio.EITHER_EDGE
+  });
+  
+    button3 = new Gpio(22, {
+    mode: Gpio.INPUT,
+    pullUpDown: Gpio.PUD_UP,
+    edge: Gpio.EITHER_EDGE
+  });
+  
+  button3.on('interrupt', function (level) {
+  if(level === 0){
+  console.log("Knop 3: ", level);
+  screens[3]();
+        }
+  });
+  
+  
+  button2.on('interrupt', function (level) {
+  if(level === 0){
+  console.log("Knop 2: ", level);
+  screens[2]();
+        }
+  });
+  
+  button1.on('interrupt', function (level) {
+      if(level === 0){
+      console.log("Knop 0: ", level);
+      screens[1]();
+            }
+      });
+  
+  button.on('interrupt', function (level) {
+  if(level === 0){
+  console.log("Knop 0: ", level);
+  screens[0]();
+        }
+  });
 
 module.exports = {start, update, stop}
